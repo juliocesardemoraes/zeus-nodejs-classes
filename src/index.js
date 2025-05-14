20eb2417422d114d1523f2e16be50f4c4d023b7c
juliocesardemoraes <jcmcf2012@gmail.com>
2025-05-14 11:36:41 -0300

    feat: add delete method class

--- src/index.js
+++ src/index.js
@@ -12,19 +12,42 @@ app.get("/", (request, response) => {
   response.status(200).send("Olá mundo");
 });
 
-app.use("/name", nameRouter);
+const PORT = 3001;
 
-app.post("/validate-password", (request, response) => {
-  const body = request.body;
+const users = [
+  { id: 1, name: "Ana", age: 25, password: "admin12345" },
+  { id: 2, name: "Carlos", age: 30, password: "adm1234" },
+];
 
-  if (body.password.length >= 6) {
-    return response.status(200).send({ message: "Senha válida" });
-  } else {
-    return response.status(400).send({ message: "Senha inválida" });
-  }
+app.get("/users", (request, response) => {
+  return response.status(200).send(users);
 });
 
-const PORT = 3001;
+app.delete("/users/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+
+  let indexToDelete = null;
+
+  for (let i = 0; i < users.length; i++) {
+    if (id == users[i].id) {
+      indexToDelete = i;
+    }
+  }
+
+  users.splice(indexToDelete, 1);
+
+  //
+  return response
+    .status(200)
+    .send({
+      message: "user deleted!",
+      messagept: "usuario deletado com sucesso!",
+      users,
+    });
+
+  // 200
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
