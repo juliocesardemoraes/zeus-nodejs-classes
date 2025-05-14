ed967d5faa65ac85a5d8b6e0c8595b29de3ad7e4
juliocesardemoraes <jcmcf2012@gmail.com>
2025-05-14 10:12:57 -0300

    fix: change put method class

--- src/index.js
+++ src/index.js
@@ -8,18 +8,46 @@ app.use(cors());
 
 app.use(express.json());
 
+let users = [
+  { id: 1, name: "Ana", age: 25, password: "admin12345" },
+  { id: 2, name: "Carlos", age: 30, password: "adm1234" },
+];
+
 app.get("/", (request, response) => {
   response.status(200).send("Olá mundo");
 });
 
-app.put("/update-user", (request, response) => {
-  const body = request.body;
-  return response.status(200).send({ message: "user updated" });
+app.get("/users", (request, response) => {
+  response.status(200).send(users);
 });
 
-app.patch("/update-user", (request, response) => {
+app.patch("/users/:id", (request, response) => {
+  const id = request.params.id;
   const body = request.body;
-  return response.status(200).send({ message: "user partially updated" });
+
+  console.log(body);
+  console.log(id);
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == id) {
+      users[i] = { ...users[i], ...body };
+    }
+  }
+
+  response.status(200).send(users);
+});
+
+app.put("/users/:id", (request, response) => {
+  const id = request.params.id;
+  const body = request.body;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == id) {
+      users[i] = { ...users[i], ...body };
+    }
+  }
+
+  response.status(200).send(users);
 });
 
 const PORT = 3001;
