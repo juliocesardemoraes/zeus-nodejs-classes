1e99247b4f8cea6041a852276a4ca2e1b77bc9d4
juliocesardemoraes <jcmcf2012@gmail.com>
2025-08-08 09:52:43 -0300

    feat: add class

--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -8,4 +8,9 @@ userRouter.get("/", async (request, response) => {
   return response.status(200).send({ working: true, users });
 });
 
+userRouter.post("/", async (request, response) => {
+  const users = await User.create(request.body);
+  return response.status(200).send({ working: true, users });
+});
+
 export { userRouter };
